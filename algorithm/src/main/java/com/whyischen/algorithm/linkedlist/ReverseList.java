0cc57fafd069f23d91da677a0919f0b29546e9ec
wangchenguang <464039958@qq.com>
2021-05-10 15:53:31 +0800

    add: 反转链表的一部分

--- algorithm/src/main/java/com/whyischen/algorithm/linkedlist/ReverseList.java
+++ algorithm/src/main/java/com/whyischen/algorithm/linkedlist/ReverseList.java
@@ -19,14 +19,12 @@ public class ReverseList {
         }
     }
 
+    private static ListNode successor;
+
     /**
      * 反转链表
-     *
-     * @param head
-     * @return
      */
     public static ListNode reverse(ListNode head) {
-
         if (head.next == null) {
             return head;
         }
@@ -39,31 +37,43 @@ public class ReverseList {
         return last;
     }
 
-    public static void main(String[] args) {
-
-        ListNode head = new ListNode(10111);
-        ListNode temp = null;
-
-        int size = 2;
-
-        for (int i = 0; i < size; i++) {
-            ListNode node = new ListNode(i);
-
-            if (temp == null) {
-                head.next = node;
-                temp = node;
-                continue;
-            }
-
-            temp.next = node;
-            temp = node;
+    /**
+     * 反转链表前N个节点
+     */
+    public static synchronized ListNode reverseN(ListNode head, int n) {
+        if (n == 1) {
+            successor = head.next;
+            return head;
         }
 
+        var last = reverseN(head.next, n - 1);
+
+        head.next.next = head;
+        head.next = successor;
+        return last;
+    }
+
+    /**
+     * 反转链表的一部分
+     */
+    public static ListNode reverseBetween(ListNode head, int m, int n) {
+        if (m == 1) {
+            return reverseN(head, n);
+        }
+
+        head.next = reverseBetween(head.next, m - 1, n - 1);
+
+        return head;
+    }
+
+
+    public static void main(String[] args) {
+
+        ListNode head = buildList(10);
+
+        head = reverseBetween(head, 2, 5);
+
         printNode(head);
-
-        ListNode reverse = reverse(head);
-
-        printNode(reverse);
     }
 
     public static void printNode(ListNode head) {
@@ -81,6 +91,27 @@ public class ReverseList {
         }
 
         System.out.println(print);
+    }
+
+    public static ListNode buildList(int size) {
+
+        ListNode head = new ListNode(0);
+
+        if (size <= 1) {
+            return head;
+        }
+
+        ListNode temp = null;
+        for (int i = 1; i < size; i++) {
+            ListNode node = new ListNode(i);
+
+            if (temp == null) temp = head;
+
+            temp.next = node;
+            temp = node;
+        }
+
+        return head;
     }
 
 }
