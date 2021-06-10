f1dfb75cd6f9c305921b6e39eb6183f281f46de1
wangchenguang <464039958@qq.com>
2021-06-10 18:12:35 +0800

    add model spring-learn

---
+++ spring-learn/src/test/java/com/whyischen/jseed/springlearn/SpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whyischen.jseed.springlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
