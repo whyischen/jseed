0706f89c1cf7ed59823b7beaa24583499ee87ae1
Chenguang Wang <464039958@qq.com>
2021-06-16 00:21:45 +0800

    add: ?

--- spring-learn/src/main/java/com/whyischen/jseed/springlearn/SpringLearnApplication.java
+++ spring-learn/src/main/java/com/whyischen/jseed/springlearn/SpringLearnApplication.java
@@ -3,8 +3,13 @@ package com.whyischen.jseed.springlearn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.reflect.Constructor;
+
 @SpringBootApplication
 public class SpringLearnApplication {
+
+    // BeanDefinition 如何加载到 ApplicationContext ?
+    // BeanFactoryPostProcessors 如何加载到 ApplicationContext ?
 
     public static void main(String[] args) {
         SpringApplication.run(SpringLearnApplication.class, args);
