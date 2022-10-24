51fe1975be9b87a1d569e3ec141f3e784b663c81
Chenguang Wang <464039958@qq.com>
2022-10-24 18:01:58 +0800

    二叉树 Node

--- algorithm/src/main/java/com/whyischen/algorithm/util/TreeNode.java
+++ algorithm/src/main/java/com/whyischen/algorithm/util/TreeNode.java
@@ -1,16 +1,14 @@
 package com.whyischen.algorithm.util;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class TreeNode {
-    private int val;
-    private TreeNode left;
-    private TreeNode right;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
 
     TreeNode(int val) {
         this.val = val;
